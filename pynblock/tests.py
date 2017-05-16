4dfee178751ea11e281ae6bd48823cd715049740
Tim Gabets <tim@gabets.ru>
2017-05-16 19:52:47 +0300

    xor() test cases

--- pynblock/tests.py
+++ pynblock/tests.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from pynblock.tools import raw2str,raw2B, B2raw
+from pynblock.tools import raw2str,raw2B, B2raw, xor
 
 class TestPynblock(unittest.TestCase):
     def test_raw2str(self):
@@ -13,6 +13,9 @@ class TestPynblock(unittest.TestCase):
 
     def test_B2raw(self):
         self.assertEqual(B2raw(b'DF1267EEDCBA9876'), b'\xdf\x12g\xee\xdc\xba\x98v')
+
+    def test_xor(self):
+        self.assertEqual(xor(b'0916101000000000', b'C19F07316463054E'), b'C88917216463054E')  
 
 if __name__ == '__main__':
     unittest.main()
