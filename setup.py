ed87aff14b69cc52426ed5b5e69359ab52b856c5
Tim Gabets <tim@gabets.ru>
2017-05-16 19:31:12 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(name='pynblock',
+      version='0.1',
+      
+      description='A payment card industry crypto library - PIN blocks, card/PIN verification values calculation etc.',
+      long_description=open('README').read(),
+      
+      classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Operating System :: OS Independent',
+        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        
+        'Topic :: Communications',
+        'Intended Audience :: Developers',
+      ],
+      
+      keywords='payment crypto card pin cvv csc pinblock',
+      
+      url='https://github.com/timgabets/pynblock',
+      author='Tim Gabets',
+      author_email='tim@gabets.ru',
+      
+      license='LGPLv2',
+      packages=['pycrypto'],
+      install_requires=[],
+      zip_safe=True)
